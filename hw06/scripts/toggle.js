b5ce6ab3dd7d43577201ae2367c56431de3c169c
greengirl6666 <greengirlstudio@gmail.com>
2017-10-23 10:04:40 -0700

    hw6

--- hw06/scripts/toggle.js
+++ hw06/scripts/toggle.js
@@ -7,6 +7,72 @@
 // on the element contains the 'active' class
 // If it does, remove the 'active' class. If it doesn't add the 'active' class.
 
+    // option 1
+
+ //  const toggleLike = () => {
+ //
+ //   if (document.getElementById('like1').classList.contains('active')) {
+ //    console.log('remove the active class')
+ //     (document.getElementById('like1').classList.remove('active'))
+ //   } else {
+ //     console.log('add the active class')
+ //     (document.getElementById('like1').classList.add('active'))
+ //   }
+ //
+ // }
+        //option 2
+
+//  const toggleLike = () => {
+//
+//     if {document.querySelectorAll('.like').classList.contaons('active')) }
+//       document.querySelectorAll('.like').forEach(function (elem) {
+//       elem.classList.remove('active')
+//     })
+//   } else {
+//       document.querySelectorAll('.like').forEach(function (elem) {
+//      elem.classList.add('active')
+//   })
+//   }
+// }
+
+        //option 3
+
+
+    const toggleLike = () => {
+
+        document.querySelectorAll('.like').forEach(function (elem) {
+         elem.classList.add('active')
+       })
+
+  console.log(document.getElementByClass('like').classList.contains('active'))
+}
+      //option 4
+
+
+  //const toggleLike = () => {
+ //
+ //   if (document.getElementByClass('like').classList.contains('active')) {
+ //    console.log('remove the active class')
+ //     (document.getElementByClass('like').classList.remove('active'))
+ //   } else {
+ //     console.log('add the active class')
+ //     (document.getElementByClass('like').classList.add('active'))
+ //   }
+ //  console.log(document.getElementByClass('like').classList.contains('active'))
+ // }
+
+     //option 5
+
+ //  const toggleLike1 = (id) => {
+ //    console.log(id)
+ //   if {document.getElementById(id).classList.contains('active') {
+ //      document.getElementById(id).classList.remove('active')
+ //   } else {
+ //      document.getElementById(id).classList.add('active')
+ //   }
+ //
+ // }
+
 
 // ------------------------------------
 // PART 2
